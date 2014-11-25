fd057ce5253cc32da5051f0f6c38400b32dbf056
Ahmed Samara <asamara@ncsu.edu>
2014-11-25 17:02:04 -0500

    correct address

--- examples/adc_test.py
+++ examples/adc_test.py
@@ -7,14 +7,14 @@ import i2c_device
 dev = i2c_device.I2CDevice(1,0x48
             ,config='adc_ads7830_i2c.yaml')
 def analog_read(self):
-        return self.registers['ADDR'].read()
+        return dev.registers['ADDR'].read()
 
 
 t0 = time.time()
 print "Begin"
 while True:
     elapsed = time.time() - t0
-    value = ADC.analog_read()
+    value = dev.analog_read()
     print "[{:8.3f}]: {:5.3f}".format(elapsed, value)
     time.sleep(0.1)
 print "Done"
