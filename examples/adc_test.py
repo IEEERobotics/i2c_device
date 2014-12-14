25375761677b18e1be87546c56686a6f67c0169e
Ahmed Samara <asamara@ncsu.edu>
2014-12-14 16:31:58 -0500

    fixed merge conflicts

--- examples/adc_test.py
+++ examples/adc_test.py
@@ -2,28 +2,23 @@
 
 import time
 import os, sys
-import i2c_device
 
-
+Updated upstream
 def analog_read():
         return dev.registers['READ'].read()
 
-dev = i2c_device.I2CDevice(1,0x48
-            ,config='adc_ads7830_i2c.yaml')
-print "test"
+import smbus
+import i2c_device
 
-"""
-enable.write('SD', 'DIFFERENTIAL')
-enable.write('CHANNEL','ONE')
-enable.write('INTERNAL_REF', 'ON')
-enable.write('AD_CONV', 'ON')
-"""
-
+bus = smbus.SMBus(1)
 t0 = time.time()
+
+
 print "Begin"
 while True:
     elapsed = time.time() - t0
     value = analog_read()
     print "Elapsed: ", elapsed, "Value: ", value
     time.sleep(0.1)
+
 print "Done"
