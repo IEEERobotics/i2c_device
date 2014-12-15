7206ff1c20796a89994eeb8cb514bef6f0b98747
Ahmed Samara <asamara@ncsu.edu>
2014-12-14 21:38:53 -0500

    added register print to adc example

--- examples/adc_test.py
+++ examples/adc_test.py
@@ -10,7 +10,8 @@ import i2c_device
 dev = i2c_device.I2CDevice(1,0x48,config='adc_ads7830_i2c.yaml')
 
 t0 = time.time()
-
+print "Registers: ", dev.registers
+print "Command: ", dev.command
 print "Begin"
 while True:
     elapsed = time.time() - t0
